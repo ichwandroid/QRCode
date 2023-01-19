e9a8c79ef709f053ddf89e2ba42384ed0f1a9b77
ichwandroid <ichwan.laruku@gmail.com>
2023-01-19 15:13:40 +0700

    asdasd

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>QRCode Scanner</title>
-  <link rel="manifest" href="manifest.json">
-</head>
-<body>
-  
-</body>
-<script>
-  window.addEventListener('load', () => {
-    registerSW();
-  });
-  
-  // Register the Service Worker
-  async function registerSW() {
-    if ('serviceWorker' in navigator) {
-      try {
-        await navigator
-        .serviceWorker
-        .register('serviceworker.js');
-      }
-      catch (e) {
-        console.log('SW registration failed');
+<html>
+  <head>
+    <title>QRCode Scanner</title>
+    <link rel="manifest" href="manifest.json">
+  </head>
+  <body>
+    This is a PWA.
+  </body>
+  <script>
+    window.addEventListener('load', () => {
+      registerSW();
+    });
+    
+    // Register the Service Worker
+    async function registerSW() {
+      if ('serviceWorker' in navigator) {
+        try {
+          await navigator
+          .serviceWorker
+          .register('serviceworker.js');
+        }
+        catch (e) {
+          console.log('SW registration failed');
+        }
       }
     }
-  }
-</script>
+  </script>
 </html>
